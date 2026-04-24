029f1b3c7201f2c7ac9666d92f451a874dec6a80
leey.lee <864032059@qq.com>
2026-04-24 23:36:42 +0800

    fix: add content for SOLID.cpp

--- src/00_Modern_Cpp_Idioms/SOLID.cpp
+++ src/00_Modern_Cpp_Idioms/SOLID.cpp
@@ -3,3 +3,14 @@
 // Liskov 里氏替换原则
 // Interface 接口隔离原则
 // Dependency 依赖倒置原则
+
+
+
+struct Journal{
+    string title;
+    vector<string> entries;
+    explicit Journal(const string& title):title{title}{}
+    // explicit可以防止隐式转换.防止Journal j = "hello";必须显示初始化.
+    // 关于这里学到的新知识点，用title{}初始化而不是传统的title().
+    // {}可以防止精度丢失，还有一致性可以告诉编译器这是在初始化.
+}
